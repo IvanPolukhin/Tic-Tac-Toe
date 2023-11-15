ad265c70b2eb3a151c8daf20ca1f803032580523
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2023-11-15 20:20:02 +0200

    js

---
+++ JS/main.js
@@ -0,0 +1,52 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+function makeMove(index) {
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        document.getElementById('board').children[index].innerText = currentPlayer;
+
+        if (checkWinner()) {
+            document.getElementById('status').innerText = `Player ${currentPlayer} wins!`;
+            gameActive = false;
+        } else if (gameBoard.every(cell => cell !== '')) {
+            document.getElementById('status').innerText = 'It\'s a tie!';
+            gameActive = false;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            document.getElementById('status').innerText = `Player ${currentPlayer}'s turn`;
+        }
+    }
+}
+
+function checkWinner() {
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return winPatterns.some(pattern => {
+        const [a, b, c] = pattern;
+        return gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c];
+    });
+}
+
+function resetGame() {
+    currentPlayer = 'X';
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+
+    document.getElementById('status').innerText = `Player ${currentPlayer}'s turn`;
+
+    const cells = document.getElementById('board').children;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+    }
+}
